a7a95c04fa80e47de0ec1f1db13123dfdb5a385c
Shruthika <shruthika@example.com>
2026-03-02 09:43:08 +0530

    add

---
+++ 4TH-SEM/Python/assignments/M01_Language_&_Programming_Basics/AST05/task.py
@@ -0,0 +1,13 @@
+from typing import List
+def Collatz_Sequence(n: int)-> List:
+   l=[]
+   while n!=1:
+       l.append(n)
+       if n%2==0:
+           n=n//2
+       else:
+           n=3*n+1
+   l.append(1)
+   return l
+if __name__ == '__main__':
+    n = int(input())
